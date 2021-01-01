0f801cf25c2c79808ad3024abb2b1a76ab8ee08e
irtep <rasanen.petri@gmail.com>
2021-01-02 00:42:15 +0200

    3.6 done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -23,12 +25,7 @@ let persons = [
     number: '39-23-6423122'
   }
 ];
-/*
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-  console.log('get received!');
-})
-*/
+
 // show all
 app.get('/api/persons', (req, res) => {
   res.json(persons);
@@ -60,6 +57,34 @@ app.delete('/api/persons/:id', (req, res) => {
   console.log('delete request for ', id);
   res.status(204).end()
 });
+// add new person
+app.post('/api/persons', (req, res) => {
+  console.log('got post: ', req.body);
+  const body = req.body;
+  let randomId = Math.floor(Math.random() * 99999);
+
+  if (!body || !body.name || !body.number) {
+    return res.status(400).json({
+      error: 'content missing'
+    });
+  }
+  // check if already in list
+  const dublicateCheck = persons.filter( person => person.name === body.name);
+  if (dublicateCheck.length === 1) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+  const note = {
+    name: body.name,
+    number: body.number,
+    id: randomId,
+  }
+
+  persons = persons.concat(note)
+  res.json(note)
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
